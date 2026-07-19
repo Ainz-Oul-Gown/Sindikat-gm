6d1daa319d2ee03f47e2f27396d8ee177ef779b7
Владислав <96347724+Ainz-Oul-Gown@users.noreply.github.com>
2026-07-19 06:07:40 +0600

    фиксы

--- vite.config.ts
+++ vite.config.ts
@@ -21,28 +21,36 @@ export default defineConfig(() => {
           theme_color: '#0f172a',
           background_color: '#0f172a',
           display: 'standalone',
-          start_url: '.',
+          display_override: ['window-controls-overlay'],
+          start_url: './',
+          scope: './',
+          protocol_handlers: [
+            {
+              protocol: 'web+syndicate',
+              url: './?url=%s'
+            }
+          ],
           icons: [
             {
-              src: 'icon-192.png',
+              src: './icon-192.png',
               sizes: '192x192',
               type: 'image/png',
               purpose: 'any'
             },
             {
-              src: 'icon-192.png',
+              src: './icon-192.png',
               sizes: '192x192',
               type: 'image/png',
               purpose: 'maskable'
             },
             {
-              src: 'icon-512.png',
+              src: './icon-512.png',
               sizes: '512x512',
               type: 'image/png',
               purpose: 'any'
             },
             {
-              src: 'icon-512.png',
+              src: './icon-512.png',
               sizes: '512x512',
               type: 'image/png',
               purpose: 'maskable'
@@ -50,13 +58,13 @@ export default defineConfig(() => {
           ],
           screenshots: [
             {
-              src: 'screenshot-wide.png',
+              src: './screenshot-wide.png',
               sizes: '1280x720',
               type: 'image/png',
               form_factor: 'wide'
             },
             {
-              src: 'screenshot-narrow.png',
+              src: './screenshot-narrow.png',
               sizes: '720x1280',
               type: 'image/png',
               form_factor: 'narrow'
